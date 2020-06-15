13d785af433294677ca3b620528e2bad21e6fa55
bionade24 <o.roesler@oscloud.info>
2020-06-15 21:50:08 +0200

    Add Hurdle.java

---
+++ src/game/Hurdle.java
@@ -0,0 +1,12 @@
+package game;
+
+import java.awt.image.BufferedImage;
+
+public class Hurdle extends GameObject {
+
+    public Hurdle(Coordinate position, double witdth, double height,
+        BufferedImage img) {
+
+        super(position, witdth, height, img);
+    }
+}
