f37fc3bc5041112414ff0f94881e13af698c3e8f
bionade24 <o.roesler@oscloud.info>
2020-10-19 12:51:29 +0200

    Set Menu background

--- src/main/java/eu/bionade24/junglejump/menu/MenuPage.java
+++ src/main/java/eu/bionade24/junglejump/menu/MenuPage.java
@@ -1,6 +1,7 @@
 package eu.bionade24.junglejump.menu;
 
 import java.awt.*;
+
 import javax.swing.JPanel;
 import eu.bionade24.junglejump.components.AdvancedButton;
 import eu.bionade24.junglejump.components.Helper;
@@ -15,7 +16,11 @@ public class MenuPage extends JPanel {
     private AdvancedButton settingsButton;
     private AdvancedButton quitButton;
 
+    private static Color backgroundColor = new Color(255, 190, 30);
+
     public MenuPage() {
+
+        this.setBackground(backgroundColor);
 
         startButton = new AdvancedButton("Start Game");
         settingsButton = new AdvancedButton("Settings");
@@ -31,6 +36,7 @@ public class MenuPage extends JPanel {
         this.add(t1);
 
         JPanel p2 = new JPanel();
+        p2.setBackground(backgroundColor);
         p2.setLayout(new GridLayout(3, 1, 0, 20));
         p2.add(startButton);
         p2.add(settingsButton);
